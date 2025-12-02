13d2f622d4a02c740401e2036b134684aede8162
Hilary Bayer <hbayer@uw.edu>
2025-12-02 12:03:59 -0800

    add functions for distance measures and 4 types of tests for these functions

--- src/semantic_similarity.py
+++ src/semantic_similarity.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import torch
 from nltk.translate.bleu_score import sentence_bleu
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import jaccard_score
 from sentence_transformers import SentenceTransformer
+from scipy.spatial import distance
+from math import acos, pi
 
 # Function to calculate the metrics for each row
 def calculate_overlap(row, string1, string2):
@@ -53,6 +56,86 @@ def calculate_embedding_cosine(row, string1, string2):
 
     return pd.Series({'Cosine_Similarity': cosine_sim})
 
-# def calculate_embedding_euclidean():
+def calculate_embedding_euclidean(row, string1, string2):
+    '''
+    Calculate euclidean distance between sentence embeddings.
+    Input: row with 'string1' and 'string2' fields defined
+    Output: Series with euclidean distance greater than or equal to 0
+    '''
+
+    sent1 = row[string1]
+    sent2 = row[string2]
+
+    if len(sent1.strip()) == 0 or len(sent2.strip()) == 0:
+        raise ValueError("Empty string input detected")
+    else:
+        pass
+
+    # Load pre-trained sentence transformer model
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+       # Generate embeddings for both sentences
+    embedding1 = model.encode([sent1], convert_to_tensor=True)
+    embedding2 = model.encode([sent2], convert_to_tensor=True)
+
+    # Calculate euclidian distance 
+    euclidean_dist = distance.euclidean(embedding1.squeeze().cpu().numpy(), embedding2.squeeze().cpu().numpy())
+
+    return pd.Series({'Euclidean_Distance': euclidean_dist})
+
+
+def calculate_embedding_manhattan(row, string1, string2):
+    '''
+    Calculate manhattan distance between sentence embeddings.
+    Input: row with 'string1' and 'string2' fields defined
+    Output: Series with manhattan distance greater than or equal to 0
+    '''
+
+    sent1 = row[string1]
+    sent2 = row[string2]
+    
+    if len(sent1.strip()) == 0 or len(sent2.strip()) == 0:
+        raise ValueError("Empty string input detected")
+    else:
+        pass
+
+    # Load pre-trained sentence transformer model
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    # Generate embeddings for both sentences
+    embedding1 = model.encode([sent1], convert_to_tensor=True)
+    embedding2 = model.encode([sent2], convert_to_tensor=True)
+
+    # Calculate manhattan distance 
+    manhattan_dist = distance.cityblock(embedding1.squeeze().cpu().numpy(), embedding2.squeeze().cpu().numpy())
+
+    return pd.Series({'Manhattan_Distance': manhattan_dist})
+
+def calculate_embedding_angular(row, string1, string2):
+    '''
+    Calculate angular distance between sentence embeddings.
+    Input: row with 'string1' and 'string2' fields defined
+    Output: Series with angular distance greater than or equal to 0
+    '''
+
+    sent1 = row[string1]
+    sent2 = row[string2]
+
+    if len(sent1.strip()) == 0 or len(sent2.strip()) == 0:
+        raise ValueError("Empty string input detected")
+    else:
+        pass
+
+    # Load pre-trained sentence transformer model
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    # Generate embeddings for both sentences
+    embedding1 = model.encode([sent1], convert_to_tensor=True)
+    embedding2 = model.encode([sent2], convert_to_tensor=True)
+
+    # Calculate euclidian distance 
+    angular_dist = acos(1 - distance.cosine(embedding1.squeeze().cpu().numpy(), embedding2.squeeze().cpu().numpy()))/pi
+
+    return pd.Series({'Angular_Distance': angular_dist})
 
 
